0a24a5d705808c28ffafa609030a720a2cd8e59f
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2020-06-08 13:53:39 +0200

    added buttons for login testing

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,3 +1,20 @@
 function load_users(){
     document.getElementById("content").innerHTML='<object type="text/html" data="Brugeroversigt/index.html" ></object>';
 }
+
+function adminlogin() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="AdminScreen/index.html" ></object>';
+}
+
+function farmalogin() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="FarmaScreen/index.html" ></object>';
+
+}
+
+function pleadlogin() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="PLeadScreen/index.html" ></object>';
+}
+
+function lablogin() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="LabScreen/index.html" ></object>';
+}
