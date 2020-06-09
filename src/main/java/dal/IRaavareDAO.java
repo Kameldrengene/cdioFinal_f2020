0be76194b849e5dace7f92f7432666564a509859
Volkan <volkanisik@live.dk>
2020-06-09 13:59:59 +0200

    interfaces!!!

--- src/main/java/dal/IRaavareDAO.java
+++ src/main/java/dal/IRaavareDAO.java
@@ -1,4 +1,15 @@
 package dal;
 
+import dal.dto.RaavareDTO;
+
+import java.util.List;
+
 public interface IRaavareDAO {
+
+    RaavareDTO getRaavare(int raavareId) throws IDALException.DALException;
+    List<RaavareDTO> getRaavareList() throws IDALException.DALException;
+    void createRaavare(RaavareDTO raavare) throws IDALException.DALException;
+    void updateRaavare(RaavareDTO raavare) throws IDALException.DALException;
+
+
 }
--- src/main/java/dal/IRaavarebatchDAO.java
+++ src/main/java/dal/IRaavarebatchDAO.java
@@ -1,4 +1,13 @@
 package dal;
 
+import dal.dto.RaavarebatchDTO;
+
+import java.util.List;
+
 public interface IRaavarebatchDAO {
+    RaavarebatchDTO getRaavarebatch(int rbId) throws IDALException.DALException;
+    List<RaavarebatchDTO> getRaavarebatchList() throws IDALException.DALException;
+    List<RaavarebatchDTO> getRaavarebatchList(int raavareId) throws IDALException.DALException;
+    void createRaavarebatch(RaavarebatchDTO raavarebatch) throws IDALException.DALException;
+    void updateRaavarebatch(RaavarebatchDTO raavarebatch) throws IDALException.DALException;
 }
