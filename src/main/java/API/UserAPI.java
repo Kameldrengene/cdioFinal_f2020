dd3d9276d9f8feb06e8c451ecd95570438fd323f
Mark-Laptop <bluedogdk@gmail.com>
2020-06-09 13:40:36 +0200

    Oprettet funktioner i UserAPI og rettet til i .js

--- src/main/java/API/UserAPI.java
+++ src/main/java/API/UserAPI.java
@@ -1,4 +1,40 @@
 package API;
 
+import controller.UserService;
+import dal.IUserDAO;
+import dal.UserDAOSQL;
+import dal.dto.UserDTO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("user")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserAPI {
+    public UserService userService = new UserService();
+    @Path("getUsers")
+    @GET
+    public UserDAOSQL getData() {
+        try {
+            return userService.getData();
+        } catch (IUserDAO.DALException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Path("activeUser")
+    @POST
+    public UserDTO activitySwitchUser(UserDTO user){
+        return userService.activitySwitchUser(user);
+    }
+    @POST
+    public UserDTO createUser(UserDTO user){
+        return userService.createUser(user);
+    }
+    @Path("updateUser")
+    @POST
+    public UserDTO updateUser(UserDTO user){
+        return userService.updateUser(user);
+    }
 }
