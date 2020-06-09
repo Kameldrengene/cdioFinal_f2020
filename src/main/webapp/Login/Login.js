44b383f9ba5473ee742f7e1094a716e4b1592d91
Mik1812 <s183913@student.dtu.dk>
2020-06-09 16:49:33 +0200

    Tilføjet radio buttons på forsiden

--- src/main/webapp/Login/Login.js
+++ src/main/webapp/Login/Login.js
@@ -1,19 +1,22 @@
 $("document").ready(function () {
     $("#administrator").click(function () {
-        PersonList();
+        PersonList(0);
     });
 });
 
-function PersonList() {
+function PersonList(role) {
 
     $.getJSON("/BoilerPlate_war_exploded/rest/live/getUsers", function(data){
         var person_data;
         $.each(data,function (key, inner) {
             $.each(inner,function (key, value) {
+
                 var userID = value.userID;
                 person_data += '<tr>';
+                person_data += "<td><input type=\"radio\" name=\"rolle\" id=\"administrator\"></td>";
                 person_data += '<td>'+userID+'</td>';
                 person_data += '<td>'+value.userName+'</td>';
+                person_data += '<tr>';
 
             });
         });
