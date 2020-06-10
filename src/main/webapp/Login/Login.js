f174f50b6bc287c761a3e480a17592e2e7459181
Mik1812 <s183913@student.dtu.dk>
2020-06-10 14:59:50 +0200

    Produkleder forside lavet

--- src/main/webapp/Login/Login.js
+++ src/main/webapp/Login/Login.js
@@ -1,12 +1,24 @@
-$.ajaxSetup({async: false});
+// $.ajaxSetup({async: false});
+
+
+
 
 $("document").ready(function () {
+
 
     //Keeps the different users stored, so they don't have to be reloaded multiple times
     PersonList("Administrator");
     PersonList("Farmaceut");
     PersonList("Produktionsleder");
     PersonList("Laborant");
+
+    // $(document).ajaxComplete(function(){
+    //     $(".loader").css("display", "none");
+    //
+    // });
+
+
+    $("#login").load("Login/PickRole.html");
 
 
     $("#administrator").click(function () {
@@ -47,7 +59,7 @@ $("document").ready(function () {
         else if(loginRole == "farma")
             switchP("FarmaScreen/index.html");
         else if(loginRole == "prodLeder")
-            switchP("PLeadScreen/index.html")
+            switchP("PLeadScreen/PLeadScreen.html")
         else if(loginRole == "laborant")
             switchP("LabScreen/index.html");
 
@@ -55,6 +67,7 @@ $("document").ready(function () {
 
     });
 });
+
 
 async function PersonList(role) {
 
