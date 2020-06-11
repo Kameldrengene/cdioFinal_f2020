89010e348cb4394674d6b78a8f367c0812643881
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2020-06-11 19:59:24 +0200

    RVService fin

--- src/main/java/API/RaavareService.java
+++ src/main/java/API/RaavareService.java
@@ -8,35 +8,35 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("Raavare")
+@Path("RV")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class RaavareService {
 
+    private RaavareController RVController = new RaavareController();
 
-    private RaavareController raavareController = new RaavareController();
-    @Path("getRaavarer")
+    @Path("getRVList")
     @GET
-    public List<RaavareDTO> getData() throws IDALException.DALException {
-        return raavareController.getRVList();
+    public List<RaavareDTO> getRVList() throws IDALException.DALException {
+        return RVController.getRVList();
     }
 
     @GET
-    @Path("getRaavare/{raavareID}")
-    public RaavareDTO getRaavare (@PathParam("raavareID") int raavareID){
-        return raavareController.gerRV(raavareID);
+    @Path("getRV/{RVID}")
+    public RaavareDTO getRV (@PathParam("RVID") int RVID){
+        return RVController.gerRV(RVID);
     }
 
     @POST
-    @Path("opretRaavare")
-    public RaavareDTO opretRaavare (RaavareDTO raavareDTO){
-        return raavareController.opretRV(raavareDTO);
+    @Path("opretRV")
+    public RaavareDTO opretRV(RaavareDTO rv){
+        return RVController.opretRV(rv);
     }
 
     @PUT
-    @Path("updaterRaavare")
-    public RaavareDTO updateRaavare (RaavareDTO raavareDTO){
-        return raavareController.opdaterRV(raavareDTO);
+    @Path("opdaterRV")
+    public RaavareDTO opdaterRV(RaavareDTO rv){
+        return RVController.opdaterRV(rv);
     }
 
 }
