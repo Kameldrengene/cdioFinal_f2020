fd6860eb23a474b42b108acc5b75401c527c217a
Mik1812 <s183913@student.dtu.dk>
2020-06-11 15:32:22 +0200

    Både "enter" og gem knap logger nu den korrekte inputværdi i råvarbatches

--- src/main/webapp/PLeadScreen/Raavarebatches/RedigerRaavarebatches.js
+++ src/main/webapp/PLeadScreen/Raavarebatches/RedigerRaavarebatches.js
@@ -4,17 +4,31 @@ $("document").ready(function () {
 
     $("#batchID").html(batchID);
 
-    $.getJSON("/BoilerPlate_war_exploded/rest/Raavarebatch/getBatch/" + batchID,function (data) {
+    $.getJSON("/BoilerPlate_war_exploded/rest/Raavarebatch/getBatch/" + batchID, function(data) {
         $("#raavareID").html(data.raavareId);
         $("#actualAmount").html(
-            "<form> " +
+            "<form onsubmit='return false;'> " +
             "<input type='text' id='newAmount' value=" + data.aktuelMaengde + ">" +
             "</form>"
-
         );
         $("#oriAmount").html(data.startMaengde);
 
     })
+
+    $("#gem").click(function () {
+        const newAmount = $("#newAmount").val();
+        console.log(newAmount);
+
+    })
+
+    $('input').keyup(function(e){
+        if(e.keyCode == 13) {
+            const newAmount = $("#newAmount").val();
+            console.log(newAmount);
+        }
+    });
+
+
 
 
 
