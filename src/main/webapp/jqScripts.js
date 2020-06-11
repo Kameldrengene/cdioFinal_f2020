394d298f190723401b27eaa9ad6dfdd0440becf7
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2020-06-11 09:51:47 +0200

    Added FarmaScripts

--- src/main/webapp/jqScripts.js
+++ src/main/webapp/jqScripts.js
@@ -25,18 +25,6 @@ function viewlist(headers, link, tableName, btnHtmlfunc) {
     });
 }
 
-function confirmRaavareUpdate(id){
-    $(document).ready(function () {
-        if(confirm("are you sure, you want to update this råvare "+ id +"?")){
-            switchP('FarmaScreen/VisRaavare/UpdateRaavare/index.html')
-            updatedID = id;
-        }
-        else {
-            alert("no worries!");
-        }
-    });
-}
-
 
 function switchP(page) {
     $("body").load(page);
