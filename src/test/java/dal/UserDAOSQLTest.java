873c2ffc4a6f4ce5f37303c8e243523a8789ed54
Volkan <volkanisik@live.dk>
2020-06-14 09:16:32 +0200

    SQLDatabaseIOTest

---
+++ src/test/java/dal/UserDAOSQLTest.java
@@ -0,0 +1,36 @@
+package dal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDAOSQLTest {
+
+    @Test
+    public void getUser() {
+    }
+
+    @Test
+    public void getRole() {
+    }
+
+    @Test
+    public void getData() {
+    }
+
+    @Test
+    public void createUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void aktivitySwitchUser() {
+    }
+
+    @Test
+    public void getActivity() {
+    }
+}
