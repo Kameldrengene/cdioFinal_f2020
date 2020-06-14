4fdf1c492a7f72543b5f7364459fad1de9b940a6
Mik1812 <s183913@student.dtu.dk>
2020-06-14 19:39:29 +0200

    Fancy fancy fancy load på forsiden

--- src/main/webapp/Login/Login.js
+++ src/main/webapp/Login/Login.js
@@ -1,109 +1,121 @@
-// $.ajaxSetup({async: false});
 
 $("document").ready(function () {
 
+    //To prevent false logins
     localStorage.setItem('loginID', 'None');
 
-    //Keeps the different users stored, so they don't have to be reloaded multiple times
-    PersonList("Administrator");
-    PersonList("Farmaceut");
-    PersonList("Produktionsleder");
-    PersonList("Laborant");
+    //Keeps the different users in local storage. So they don't have to be reloaded multiple times
+    //.then to ensure functionality is only added after resources are fetched
+    loadUsers().then(function () {
 
-    // $(document).ajaxComplete(function(){
-    //     $(".loader").css("display", "none");
-    //
-    // });
+        $("#administrator").click(function () {
+            localStorage.setItem("loginRole", "admin");
+            localStorage.setItem('loginID', 'None');
+            const admins = localStorage.getItem("Administrator");
+            $("#personer").html(admins);
+        });
 
-    $("#login").load("Login/PickRole.html");
+        $("#farmaceut").click(function () {
+            localStorage.setItem("loginRole", "farma");
+            localStorage.setItem('loginID', 'None');
+            const farmas = localStorage.getItem("Farmaceut");
+            $('#personer').html(farmas);
+        });
 
-    $("#administrator").click(function () {
-        localStorage.setItem("loginRole", "admin");
-        localStorage.setItem('loginID', 'None');
-        var admins = localStorage.getItem("Administrator");
-        $("#personer").html(admins);
+        $("#produktionsleder").click(function () {
+            localStorage.setItem("loginRole", "prodLeder");
+            localStorage.setItem('loginID', 'None');
+            const prodLeaders = localStorage.getItem("Produktionsleder");
+            $('#personer').html(prodLeaders);
+        });
+
+        $("#laborant").click(function () {
+            localStorage.setItem("loginRole", "laborant");
+            localStorage.setItem('loginID', 'None');
+            const labos = localStorage.getItem("Laborant");
+            $('#personer').html(labos);
+        });
+
+        //Saves the ID of the selected user in localStorage
+        $("#table").on("click", "input", function () {
+            localStorage.setItem("loginID", this.id);
+        });
+
+        //Only load user data and sign in button when everything is ready
+        $("#table").load("Login/BrugerTabel.html")
+        $("#signin").html("<button class='hvr-buzz'>Sign in</button>");
+
+        //Switches to the right page when "sing in" is pressed
+        $(".hvr-buzz").click(function () {
+            const loginRole = localStorage.getItem("loginRole");
+            const ID = localStorage.getItem("loginID");
+
+            if (ID === "None") {
+                alert("Vælg venligst en rolle")
+            } else {
+                if (loginRole == "admin")
+                    switchP("AdminScreen/index.html");
+                else if (loginRole == "farma")
+                    switchP("FarmaScreen/index.html");
+                else if (loginRole == "prodLeder")
+                    switchP("PLeadScreen/PLeadScreen.html")
+                else if (loginRole == "laborant")
+                    switchP("LabScreen/index.html");
+            }
+        });
     });
 
-    $("#farmaceut").click(function () {
-        localStorage.setItem("loginRole", "farma");
-        localStorage.setItem('loginID', 'None');
-        var farmas = localStorage.getItem("Farmaceut");
-        $('#personer').html(farmas);
-    });
-
-    $("#produktionsleder").click(function () {
-        localStorage.setItem("loginRole", "prodLeder");
-        localStorage.setItem('loginID', 'None');
-        var prodLeaders = localStorage.getItem("Produktionsleder");
-        $('#personer').html(prodLeaders);
-    });
-
-    $("#laborant").click(function () {
-        localStorage.setItem("loginRole", "laborant");
-        localStorage.setItem('loginID', 'None');
-        var labos = localStorage.getItem("Laborant");
-        $('#personer').html(labos);
-    });
-
-    //Saves the ID of the selected user in localStorage
-    $(".brugertable").on("click", "input", function () {
-        localStorage.setItem("loginID", this.id);
-    })
-
-    //Switches tjo the right page when "sing in" button is pressed
-    $(".hvr-buzz").click(function () {
-        var loginRole = localStorage.getItem("loginRole");
-        var ID = localStorage.getItem("loginID");
-
-        if (ID === "None") {
-            alert("Vælg venligst en rolle")
-        } else {
-            if (loginRole == "admin")
-                switchP("AdminScreen/index.html");
-            else if (loginRole == "farma")
-                switchP("FarmaScreen/index.html");
-            else if (loginRole == "prodLeder")
-                switchP("PLeadScreen/PLeadScreen.html")
-            else if (loginRole == "laborant")
-                switchP("LabScreen/index.html");
-        }
-
-    });
 
 });
 
+async function loadUsers(){
 
-async function PersonList(role) {
+    //Display loader
+    $("#loader").html("<div id='loading'><\div")
 
+    //fetch and save user data
+    await loadUser("Administrator");
+    await loadUser("Farmaceut");
+    await loadUser("Produktionsleder");
+    await loadUser("Laborant");
+
+    //Remove loader
+    $("#loader").html("")
+
+}
+
+async function loadUser(role) {
+
+    await $.ajax({
+        url: "/BoilerPlate_war_exploded/rest/user/getRole?role=" + role,
+        type: "GET",
+        async: true,
+        contentType: "application/json",
+        dataType: "json",
+        success: data => createTable(data, role)
+    });
+
+}
+
+function createTable(data, role){
     //Variable to hold all the tabel rows
-    var tabelData = "";
+    let tabelData = "";
 
-    //Get all the persons
-    await $.getJSON("/BoilerPlate_war_exploded/rest/user/getRole?role=" + role).then(function (data) {
+    //Loop through
+    $.each(data, function (key, value) {
+        if (value.aktiv) {
+            const userID = value.userID;
 
-        //Loop through
-
-        if(role === "Administrator"){
+            //Uses userID for label reference
             tabelData += "<tr>";
-            tabelData += "<td><input type = 'radio' name = 'rolle' id ='0'></td>";
-            tabelData += "<td><Label for ='0'>0</Label></td>";
-            tabelData += "<td><Label for ='0'>root</Label></td>";
+            tabelData += "<td><input type = 'radio' name = 'rolle' id ='" + userID + "'></td>";
+            tabelData += "<td><Label for ='" + userID + "'>" + userID + "</Label></td>";
+            tabelData += "<td><Label for ='" + userID + "'>" + value.userName + "</Label></td>";
             tabelData += "</tr>";
         }
-        $.each(data, function (key, value) {
-            if (value.aktiv) {
-                var userID = value.userID;
+    });
+
+    localStorage.setItem(role, tabelData)
+}
 
 
-                //Uses userID for label reference
-                tabelData += "<tr>";
-                tabelData += "<td><input type = 'radio' name = 'rolle' id ='" + userID + "'></td>";
-                tabelData += "<td><Label for ='" + userID + "'>" + userID + "</Label></td>";
-                tabelData += "<td><Label for ='" + userID + "'>" + value.userName + "</Label></td>";
-                tabelData += "</tr>";
-            }
-        });
-    })
-
-    localStorage.setItem(role, tabelData);
-};
