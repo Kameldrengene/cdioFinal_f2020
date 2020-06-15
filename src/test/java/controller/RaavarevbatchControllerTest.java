21f1e48e4727715fcb07bf841894dbe68d9e7404
Volkan <volkanisik@live.dk>
2020-06-15 12:05:38 +0200

    RaavarebatchDAOSQLTest

---
+++ src/test/java/controller/RaavarevbatchControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaavarevbatchControllerTest {
+
+    @Test
+    void getData() {
+    }
+
+    @Test
+    void getAktuelle() {
+    }
+
+    @Test
+    void getBatch() {
+    }
+
+    @Test
+    void updateRaavarebatch() {
+    }
+
+    @Test
+    void opretRaavarebatch() {
+    }
+}
