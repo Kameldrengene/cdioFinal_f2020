a84365dbc961a0f16cb39e767d715e10b4c920e7
Mik1812 <s183913@student.dtu.dk>
2020-06-16 12:01:09 +0200

    Opret råvarebatch done til de nye krav

--- src/main/webapp/PLeadScreen/Raavarebatches/OpretRaavarebatch/AngivData.js
+++ src/main/webapp/PLeadScreen/Raavarebatches/OpretRaavarebatch/AngivData.js
@@ -1,40 +1,31 @@
 $("document").ready(function () {
 
-    const raavareID = localStorage.getItem("activeRaavare");
+    const activeRaavareID = localStorage.getItem("activeRaavare");
 
-    $("#raavareID").html("Råvare ID: " + raavareID);
+    $("#raavareID").html("Råvare ID: " + activeRaavareID);
 
-    // loadBatch(batchID);
-    //
-    // $("#produktbatch").on("click", "button", function () {
-    //     localStorage.setItem("activeRBId", this.id);
-    //     switchP("PLeadScreen/Produktbatches/RedigerProduktbatch.html");
-    // })
 
 })
 
+$("#opret").click(function () {
 
+    const activeRaavareID = localStorage.getItem("activeRaavare");
+    const activeBatchID = $('#batchID').val();
+    const activeStartMaengde = $('#startMaengde').val();
 
-
-
-
-
-
-
-$("#gem").click(function () {
-
-    const batchID = $('#batchID').val();
-    const raavareID = $('#raavareID').val();
-    const mængde = $('#aktuelMaengde').val();
-    const oprindeligMaengde = $('#oprindeligMaengde').val();
-
-    var obj = { rbId: batchID, raavareId: raavareID, aktuelMaengde: mængde, startMaengde: oprindeligMaengde };
+    var obj = { rbId: activeBatchID, raavareId: activeRaavareID, aktuelMaengde: activeStartMaengde, startMaengde: activeStartMaengde };
     var myJson = JSON.stringify(obj);
+
     sendAjax("/BoilerPlate_war_exploded/rest/Raavarebatch/opretRaavarebatch", function (data) {
         alert("Råvarebatch oprettet succesfuldt");
         $("#gem").removeAttr("hover");
+        switchP("PLeadScreen/PLeadScreen.html")
     }, function (data) {
         alert("Error making RaavareBatch: ERR.NO.08");
         console.log(data);
     }, "POST", myJson);
+
+
+
+
 })
