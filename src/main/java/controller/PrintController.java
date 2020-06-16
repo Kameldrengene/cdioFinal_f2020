028106d743a8401322d7a18f934d8fb18c5f4e23
Volkan <volkanisik@live.dk>
2020-06-16 23:37:34 +0200

    PrintService

---
+++ src/main/java/controller/PrintController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import dal.IDALException;
+import dal.PrintDAOSQL;
+import dal.dto.PrintDTO;
+
+import java.util.List;
+
+public class PrintController {
+    PrintDAOSQL printDAOSQL;
+    public PrintController(){
+        printDAOSQL = new PrintDAOSQL();
+    }
+
+    public List<PrintDTO> getData(int pbID, int receptID){
+        try {
+            return printDAOSQL.getPrint(pbID,receptID);
+        } catch (IDALException.DALException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
