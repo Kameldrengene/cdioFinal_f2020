74ec3355bcbdbe27aceeaf1c16da2915bc6446a6
Talha-Butt786 <s195475@student.dtu.dk>
2020-06-16 23:39:06 +0200

    under process recept

--- src/main/webapp/FarmaScripts.js
+++ src/main/webapp/FarmaScripts.js
@@ -67,12 +67,17 @@ function postRaavareUpdate() {
 
 }
 
-function ingenDublicate() {
+function ingenDublicate() {  //funktion checker for samtlige raavar navn.
     var checkraavar = [];
-    for (let i = 1; i <=counter ; i++) {
-        const RaaNavn = document.getElementById('ledeligeNavn'+i+'').value;
-        checkraavar.push(RaaNavn);
+    for (let i = 1; i <= counter ; i++) {
+        if($('#ledeligeNavn'+i+'').length) {
+            console.log(i);
+            const RaaNavn = document.getElementById('ledeligeNavn' + i + '').value;
+            checkraavar.push(RaaNavn);
+        }
     }
+
+    console.log(checkraavar);
 
     for (let i = 0; i < checkraavar.length ; i++) {
         for (let j = i+1; j < checkraavar.length; j++) {
@@ -132,6 +137,7 @@ function opretRecept() {
                 }
             },
             error: function (jqXHR, text, error) {
+                i = i-1;
                 alert(jqXHR + text.status);
             }
         });
@@ -223,36 +229,6 @@ function confirmReceptupdate(raavareID) {
     });
 }
 
-function gemAlt(raavareID) {
-    $("document").ready(function () {
-        switchP("FarmaScreen/VisRecept/updateRecept.html");
-        const ReceptId = localStorage.getItem("vistID");
-        const RaavarID = localStorage.getItem("raavarid");
-
-        $("#ReceptID").html(ReceptId);
-
-        $.getJSON("/BoilerPlate_war_exploded/rest/Recept/getRecept/" + ReceptId + "/" + RaavarID, function (data) {
-            $("#ReceptNavn").html(data.receptNavn);
-            $("#RåvareID").html(data.raavareId);
-            $("#nonNetto").html(data.nonNetto);
-            $("#Tolerance").html(data.tolerance);
-
-        });
-
-        $("#gem").click(function () {
-            if(confirm("gem ændringer?")){
-                gemopdatering();
-            }else{
-                alert("ingen problem");
-            }
-        });
-
-        $("table").keypress(function (e) {
-            return e.which != 13;
-        });
-
-    });
-}
 
 function postReceptUpdate() {
     $(document).ready(function () {
@@ -479,22 +455,46 @@ function OpretReceptIDNavn() {
 }
 var counter = 0;  //bruges til at give at tilføje id's til elementer.
 function addLinje() {
-    //  listofRaavare();
-    ledeligeRaavare();
-    // checkRaavareID();
-    counter++;
-    console.log(counter);
-    document.getElementById("raavareLinjer" + counter+"").innerHTML +=
-        '<div id="raavareLinjer'+counter+'">'+
-        '<select name="RaavarID"  id="ledeligeNavn'+counter+'">' +
-        '<option value="" disabled selected>Vælg Råvare</option>' +
-        '</select><br>' +
-        '<input type="number" id="netto'+counter+'" placeholder="Mængde 1.0001" value="" step="0.0001" max="20" min="0.05"><br>' +
-        '<input type="number" id="tol'+counter+'"  placeholder="Tolerance 0.1" value="" step="0.01" max="10" min="0.1"><br> '+
-        '</div>' +
-        '<div id="raavareLinjer'+(counter + 1)+'"></div>'
-    ;
-    selectbtn(counter);
+    if(($("#recepID").val() === null) || ($("#recepnavn").val() === '')){
+        alert('udfyld alle felter');
+    }else {
+        ledeligeRaavare();
+            counter++;
+            console.log(counter);
+            var coutnum = counter;
+            console.log(coutnum);
+            $("#raavareLinjer").append(
+                '<tr id="raavareLines' + counter + '">\n' +
+                '        <td id="RåvareID">' +
+                '<select name="RaavarID"  id="ledeligeNavn' + counter + '">' +
+                '<option value="" disabled selected>Vælg Råvare</option>' +
+                '</select>' +
+                '</td>\n' +
+                '        <td>' +
+                '<input type="number" id="netto' + counter + '" placeholder="Mængde 1.0001" value="" step="0.0001" max="20" min="0.05">' +
+                '</td>\n' +
+                '        <td>' +
+                '<input type="number" id="tol' + counter + '"  placeholder="Tolerance 0.1" value="" step="0.01" max="10" min="0.1"> ' +
+                '</td>\n' +
+                '<td>\n' +
+                '            <button class="hvr-buzz" onclick="fjernRaavar('+coutnum+');" id="fjern">fjern</button>\n' +
+                '        </td>' +
+                '    </tr>');
+            console.log("test1");
+            coutnum = 0;
+            console.log(coutnum);
+        selectbtn(counter);
+    }
+}
+
+
+function fjernRaavar(counter1) {
+    $(document).ready(function () {
+        $("#raavareLines"+counter1+"").remove();
+        console.log(counter);
+      //  document.getElementById("raavareLinjer"+counter+"").innerHTML +=
+           // '<div id="raavareLinjer'+(counter + 1)+'"></div>'
+    });
 
 }
 
@@ -566,58 +566,10 @@ function addRecept() {
     });
 }
 
-
-
-
-
-
-$("document").ready(function(){
-    updateTable();
-    $("#visAfsluttede").click(function () {
-        updateTable();
-    })
-    $("#produktbatches").on("click", "button", function () {
-        localStorage.setItem("activePBId", this.id);
-        //Get content of table
-        const currentRow = $(this).closest("tr");
-        const receptID = currentRow.find("td:eq(1)").text();
-        const status = currentRow.find("td:eq(2)").text();
-        //Save content
-        localStorage.setItem("activeStatus", status);
-        localStorage.setItem("activeReceptID", receptID);
-        switchP("PLeadScreen/Produktbatches/AabenProduktbatch.html");
-    })
-});
-function updateTable(){
-    let path;
-    if($("#visAfsluttede").is(":checked"))
-        path = "getAlle";
-    else
-        path = "getAktuelle"
-    sendAjax("/BoilerPlate_war_exploded/rest/produktbatch/" + path, function (data) {
-        viewTable(data)
-    }, function (data) {
-        alert("Error getting all/actual produktbatches: ERR.NO.16");
-        console.log(data);
-    })
-};
-function viewTable(data){
-    //Variable to hold all the tabel rows
-    let tabelData = "";
-    tabelData += "<tr>";
-    tabelData += "<th>Batch ID</th>";
-    tabelData += "<th>Recept ID</th>";
-    tabelData += "<th>Status</th>";
-    tabelData += "</tr>";
-    //Loop through
-    $.each(data, function (key, value) {
-        //Uses userID for label reference
-        tabelData += "<tr>";
-        tabelData += "<td>"+value.pbId+"</td>";
-        tabelData += "<td>"+value.receptId+"</td>";
-        tabelData += "<td>"+value.status+"</td>";
-        tabelData += "<td> <button class = hvr-buzz id =" + value.pbId + " >Åben</button>  </td>"
-        tabelData += "</tr>";
+function retur() {
+    $(document).ready(function () {
+        counter = 0;
+        switchP('FarmaScreen/index.html');
     });
-    $("#produktbatches").html(tabelData);
+
 }
