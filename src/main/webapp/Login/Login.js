e2dd633711cf2e02686991fbc8d373a9b8457168
Mik1812 <s183913@student.dtu.dk>
2020-06-16 23:03:37 +0200

    Bug fix med reload på forsiden

--- src/main/webapp/Login/Login.js
+++ src/main/webapp/Login/Login.js
@@ -1,77 +1,78 @@
 
-$("document").ready(function () {
+$("document").ready(async function () {
 
     //To prevent false logins
     localStorage.setItem('loginID', 'None');
 
     //Keeps the different users in local storage. So they don't have to be reloaded multiple times
-    //.then to ensure functionality is only added after resources are fetched
-    loadUsers().then(function () {
+    //await to ensure functionality is only added after resources are fetched
+    await loadUsers();
 
-        //Only load user data and sign in button when everything is ready
-        $("#login").load("Login/LoginTable.html");
-        $(".brugertable").hide();
+    //Only load user data and sign in button when everything is ready
+    $("#login").load("Login/LoginTable.html");
+    $(".brugertable").hide();
 
-        $(".rolletabel").click(function () {
-            $(".brugertable").show();
-        })
+    $(".rolletabel").click(function () {
+        $(".brugertable").show();
+    })
 
-        //Saves the ID of the selected user in localStorage
-        $("#personer").on("click", "input", function () {
-            localStorage.setItem("loginID", this.id);
-        });
+    //Saves the ID of the selected user in localStorage
+    $("#personer").on("click", "input", function () {
+        localStorage.setItem("loginID", this.id);
+    });
 
-        //Attach the appropiate actions to the role tabel
-        $("#administrator").click(function () {
-            localStorage.setItem("loginRole", "admin");
-            localStorage.setItem('loginID', 'None');
-            const admins = localStorage.getItem("Administrator");
-            $("#personer").html(admins);
-        });
+    //Attach the appropiate actions to the role tabel
+    $("#administrator").click(function () {
+        localStorage.setItem("loginRole", "admin");
+        localStorage.setItem('loginID', 'None');
+        const admins = localStorage.getItem("Administrator");
+        $("#personer").html(admins);
+    });
 
-        $("#farmaceut").click(function () {
-            localStorage.setItem("loginRole", "farma");
-            localStorage.setItem('loginID', 'None');
-            const farmas = localStorage.getItem("Farmaceut");
-            $('#personer').html(farmas);
-        });
+    $("#farmaceut").click(function () {
+        localStorage.setItem("loginRole", "farma");
+        localStorage.setItem('loginID', 'None');
+        const farmas = localStorage.getItem("Farmaceut");
+        $('#personer').html(farmas);
+    });
 
-        $("#produktionsleder").click(function () {
-            localStorage.setItem("loginRole", "prodLeder");
-            localStorage.setItem('loginID', 'None');
-            const prodLeaders = localStorage.getItem("Produktionsleder");
-            $('#personer').html(prodLeaders);
-        });
+    $("#produktionsleder").click(function () {
+        localStorage.setItem("loginRole", "prodLeder");
+        localStorage.setItem('loginID', 'None');
+        const prodLeaders = localStorage.getItem("Produktionsleder");
+        $('#personer').html(prodLeaders);
+    });
 
-        $("#laborant").click(function () {
-            localStorage.setItem("loginRole", "laborant");
-            localStorage.setItem('loginID', 'None');
-            const labos = localStorage.getItem("Laborant");
-            $('#personer').html(labos);
-        });
+    $("#laborant").click(function () {
+        localStorage.setItem("loginRole", "laborant");
+        localStorage.setItem('loginID', 'None');
+        const labos = localStorage.getItem("Laborant");
+        $('#personer').html(labos);
+    });
 
-        //Switches to the right page when "sing in" is pressed
-        $(".hvr-buzz").click(function () {
-            const loginRole = localStorage.getItem("loginRole");
-            const ID = localStorage.getItem("loginID");
+    //Switches to the right page when "sing in" is pressed
+    $(".hvr-buzz").click(function () {
+        const loginRole = localStorage.getItem("loginRole");
+        const ID = localStorage.getItem("loginID");
 
-            if (ID === "None") {
-                alert("Vælg venligst en rolle")
-            } else {
-                if (loginRole == "admin")
-                    switchP("AdminScreen/index.html");
-                else if (loginRole == "farma")
-                    switchP("FarmaScreen/index.html");
-                else if (loginRole == "prodLeder")
-                    switchP("PLeadScreen/PLeadScreen.html")
-                else if (loginRole == "laborant")
-                    switchP("LabScreen/index.html");
-            }
-        });
-    }); //todo tilføj catch?
+        if (ID === "None") {
+            alert("Vælg venligst en rolle"); return false;
+        } else {
+            if (loginRole == "admin")
+                switchP("AdminScreen/index.html");
+            else if (loginRole == "farma")
+                switchP("FarmaScreen/index.html");
+            else if (loginRole == "prodLeder")
+                switchP("PLeadScreen/PLeadScreen.html")
+            else if (loginRole == "laborant")
+                switchP("LabScreen/index.html");
+        }
+    });
 });
 
 async function loadUsers(){
+
+    console.log("her")
 
     //Display loader
     $("#loader").html("<div id='loading'></div>")
