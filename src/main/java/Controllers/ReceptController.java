9ed952398c1819bef55b1f4ea7b5d6a57b1f756a
Mik1812 <s183913@student.dtu.dk>
2020-06-20 00:47:53 +0200

    Receptcontroller streamlined

--- src/main/java/Controllers/ReceptController.java
+++ src/main/java/Controllers/ReceptController.java
@@ -10,7 +10,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class ReceptController {
+
     public final ReceptDAOSQL receptDAOSQL;
+    private final String SQLErrorMsg = "ERROR: Fejl i forbindelse med kontakt af databasen";
 
     public ReceptController(){
         receptDAOSQL = new ReceptDAOSQL();
@@ -20,60 +22,64 @@ public class ReceptController {
         try {
             return receptDAOSQL.getReceptKomponent(receptID,raavareID);
         }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
-        return null;
     }
     public List<ReceptDTO> getData()  {
         try {
             return receptDAOSQL.getReceptList();
         }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
-        return null;
     }
 
     public List<ReceptDTO> getuniqueRecept (int receptId) {
         try {
             return receptDAOSQL.getRecepts(receptId);
         }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
-        return null;
     }
 
+    //todo beskeder skal komme fra func-lag
     public ReceptDTO opretRecept (ReceptDTO recept, int check){
+
         try {
             ReceptFunc receptFunc = new ReceptFunc();
+
             if(!(check !=0 || receptFunc.isReceptOk(recept))){
-                throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity("Error! Tilføje venligst rigtig størrelser: \n navn: >2 og <3\n"+"nonNetto: >= 0,05 og <20\n"+"tolerance: >= 0.1 og <20").build());
+
+                throw buildError(Response.Status.NOT_ACCEPTABLE, "Error! Tilføje venligst rigtig størrelser: \n navn: >2 og <3\n"+"nonNetto: >= 0,05 og <20\n"+"tolerance: >= 0.1 og <20");
             }
+
             if(!(check != 0 || !receptFunc.doesIdExist(recept, getData()))){
-                throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity("ID existerer allerede").build());
+                throw buildError(Response.Status.NOT_ACCEPTABLE, "ID existerer allerede");
             } else {
                 receptDAOSQL.createRecept(recept);
             }
+
         }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
         return recept;
     }
 
     public List<ReceptDTO> opretRecept (List<ReceptDTO> recept){
+
         try {
             int i = 0;
             int j = 0;
                 while (i < recept.size()){
                     ReceptFunc receptFunc = new ReceptFunc();
                     if (!(receptFunc.isReceptOk(recept.get(i)))) {
-                        throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity("Error! Tilføje venligst rigtig størrelser: \n navn: => 2 og < 30\n" + "nonNetto: => 0.05 og < 20\n" + "tolerance: => 0.1 og < 20").build());
+                        throw buildError(Response.Status.NOT_ACCEPTABLE, "Error! Tilføje venligst rigtig størrelser: \n navn: => 2 og < 30\n" + "nonNetto: => 0.05 og < 20\n" + "tolerance: => 0.1 og < 20");
                     }
                     i++;
                 }
                 while (j < recept.size()) {
                     ReceptFunc receptFunc = new ReceptFunc();
                     if ((receptFunc.doesIdExist(recept.get(j), getData()))) {
-                        throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity("ID existerer allerede \n Vælg en anden!").build());
+                        throw buildError(Response.Status.NOT_ACCEPTABLE, "ID existerer allerede \n Vælg en anden!");
                     }
                     j++;
                 }
@@ -81,12 +87,12 @@ public class ReceptController {
                 for (int k = 0; k < recept.size(); k++) {
                     ReceptFunc receptFunc = new ReceptFunc();
                     if ((receptFunc.doesNameExist(recept.get(k),getData()))) {
-                        throw new WebApplicationException(Response.status(Response.Status.NOT_ACCEPTABLE).entity("Navn existerer allerede \n Vælg en anden!").build());
+                        throw buildError(Response.Status.NOT_ACCEPTABLE, "Navn existerer allerede \n Vælg en anden!");
                     }
                 }
                 receptDAOSQL.createReceptList(recept);
         }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
         return recept;
     }
@@ -99,10 +105,15 @@ public class ReceptController {
                 receptDAOSQL.updateRecept(recept);
             }
             }catch (SQLException e){
-            e.printStackTrace();
+            throw buildError(Response.Status.NOT_ACCEPTABLE, SQLErrorMsg);
         }
         return recept;
     }
+
+    public WebApplicationException buildError(Response.Status status, String msg){
+        return new WebApplicationException(Response.status(status).entity(msg).build());
+    }
+
 }
 
 
