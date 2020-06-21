f6f299bd77be5fe89829166f9053e535154bd286
Mik1812 <s183913@student.dtu.dk>
2020-06-21 13:11:58 +0200

    Arbejd som Logud/tilbage knapper debuggede

---
+++ src/main/webapp/ProduktScreen/ProduktScreen.js
@@ -0,0 +1,17 @@
+$("document").ready(function () {
+
+    const loginRole = localStorage.getItem("loginRole");
+
+    //Load the correct button: "logud" or "tilbage". Depending what role is logged in.
+    if(loginRole.localeCompare("prodLeder") != 0){
+
+        //Then return page must be Farma
+        const returnPage = "'FarmaScreen/index.html'";
+        $(".logudEllerTilbage").html('<input id="logoutbtn" type="submit" class="hvr-buzz screenbtn" value="Tilbage" onclick="switchP(' + returnPage + ')">');
+        $(".menubar").hide();
+
+    } else{
+        $(".logudEllerTilbage").html('<input id="logoutbtn" type="submit" class="hvr-buzz screenbtn" value="Log ud" onclick="window.location.reload()">')
+    }
+
+})
