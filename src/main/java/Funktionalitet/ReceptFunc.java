36591f5f6be9bd20f90fba82a9a6e2dee7e26a77
Volkan <volkanisik@live.dk>
2020-06-22 17:08:49 +0200

    Comments på functionalitet

--- src/main/java/Funktionalitet/ReceptFunc.java
+++ src/main/java/Funktionalitet/ReceptFunc.java
@@ -5,6 +5,12 @@ import dal.dto.ReceptDTO;
 import java.util.List;
 
 public class ReceptFunc {
+    /**
+     * Sender beskeder i forhold til de fejl der opstår
+     * @param recept Recep data transfer objekt
+     * @param receptDTOList Recept listen
+     * @return Returnerer besked hvis der er en fejl
+     */
     public String receptmsg( ReceptDTO recept,List<ReceptDTO> receptDTOList){
         if (!isIDOk(recept)) {
             return "ID størrelse skal være mellem 1 til 99999999 decimaler";
@@ -26,21 +32,59 @@ public class ReceptFunc {
         }
         return null;
     }
+
+    /**
+     * KOntrollerer om navnet eksisterer allerede
+     * @param recept Recep data transfer objekt
+     * @param receptDTOList Recept listen
+     * @return true hvis krav opfyldt
+     */
     public boolean isReceptOk( ReceptDTO recept,List<ReceptDTO> receptDTOList){
         return (!doesNameExist(recept,receptDTOList)&& !doesIdExist(recept,receptDTOList) && isIDOk(recept) && isNameOk(recept) && isNonNettoOk(recept) && isToleranceOk(recept));
     }
+
+    /**
+     * Kontrollerer recept ID
+     * @param recept Recep data transfer objekt
+     * @return true hvis krav opfyldt
+     */
     private boolean isIDOk(ReceptDTO recept) {
         return recept.getReceptId() <= 99999999 && recept.getReceptId() >= 1;
     }
+
+    /**
+     * Kontrollerer om navnet består af 1 til 20 tegn
+     * @param recept Recep data transfer objekt
+     * @return true hvis krav opfyldt
+     */
     private boolean isNameOk(ReceptDTO recept){
         return (!(recept.getReceptNavn().length() <= 1 || recept.getReceptNavn().length() > 20));
     }
+
+    /**
+     * Kontrollerer om recept nonNetto er indenfor 0.04 til 20.0
+     * @param recept Recep data transfer objekt
+     * @return true hvis krav opfyldt
+     */
     private boolean isNonNettoOk(ReceptDTO recept){
         return (!(recept.getNonNetto()<=0.04 || recept.getNonNetto() > 20.0));
     }
+
+    /**
+     * Kontrollerer om recept Tolerance er indenfor 0.09 og 10
+     * @param recept Recep data transfer objekt
+     * @return true hvis krav opfyldt
+     */
     private boolean isToleranceOk(ReceptDTO recept){
         return (!(recept.getTolerance()<=0.09 || recept.getTolerance() > 10.0));
     }
+
+    /**
+     * KOntrollerer om Recept Id eksisterer allerede
+     * @param recept Recep data transfer objekt
+     * @param receptDTOList Recept listen
+     * @return true hvis krav opfyldt
+     */
     public boolean doesIdExist(ReceptDTO recept, List<ReceptDTO> receptDTOList){
         for (int i = 0; i < receptDTOList.size() ; i++) {
             if (receptDTOList.get(i).getReceptId()==recept.getReceptId()) {
@@ -51,6 +95,12 @@ public class ReceptFunc {
 
     }
 
+    /**
+     * Kontrollerer om navnet eksisterer allerede
+     * @param recept Recep data transfer objekt
+     * @param receptDTOList Recept listen
+     * @return true hvis krav opfyldt
+     */
     public boolean doesNameExist(ReceptDTO recept, List<ReceptDTO> receptDTOList){
         for (int i = 0; i < receptDTOList.size() ; i++) {
             if (receptDTOList.get(i).getReceptNavn().equals(recept.getReceptNavn())) {
